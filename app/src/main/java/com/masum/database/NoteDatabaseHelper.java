f68d46f7feca6530daf7216d3cfa466f2125b637
masum <masum.islam@fnxsolutions.com>
2015-08-02 19:04:23 +0300

    Database added

---
+++ app/src/main/java/com/masum/database/NoteDatabaseHelper.java
@@ -0,0 +1,29 @@
+package com.masum.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by masum on 02/08/15.
+ */
+public class NoteDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "noteTable.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+    public NoteDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        NoteTable.onCreate(database);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+        NoteTable.onUpgrade(database, oldVersion, newVersion);
+    }
+}
---
+++ app/src/main/java/com/masum/database/NoteTable.java
@@ -0,0 +1,60 @@
+package com.masum.database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.masum.locationnote.MainActivity;
+
+/**
+ * Created by masum on 02/08/15.
+ */
+public class NoteTable {
+
+    // Database table
+    public static final String TABLE_NOTE = "note";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_DESCRIPTION = "description";
+    public static final String COLUMN_DATE = "date";
+    public static final String COLUMN_LATITUDE = "latitude";
+    public static final String COLUMN_LONGITUDE = "longitue";
+    public static final String COLUMN_IMAGE = "image";
+
+
+    // Database creation SQL statement
+    private static final String DATABASE_CREATE = "CREATE TABLE " +
+            TABLE_NOTE
+            + "( "
+            + COLUMN_ID + " INTEGER PRIMARY  KEY AUTOINCREMENT, "
+            + COLUMN_TITLE  +" VARCHAR(255) NOT NULL, "
+            + COLUMN_DESCRIPTION + " VARCHAR(1024), "
+            + COLUMN_DATE + " VARCHAR(255) NOT NULL,"
+            + COLUMN_LATITUDE + " VARCHAR(30),"
+            + COLUMN_LONGITUDE + " VARCHAR(30),"
+            + COLUMN_IMAGE + " VARCHAR(255)"
+            +");";
+    // Database drop table SQL statement
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NOTE;
+
+
+    public static void onCreate(SQLiteDatabase database) {
+        try {
+            database.execSQL(DATABASE_CREATE);
+            Log.i(MainActivity.TAG, "table created successfully");
+        }catch (Exception e) {
+            //System.out.println("ex: "+e.getMessage());
+            Log.i(MainActivity.TAG, "exception: "+e.getMessage());
+        }
+
+    }
+
+    public static void onUpgrade(SQLiteDatabase database, int oldVersion,
+                                 int newVersion) {
+        Log.w(MainActivity.TAG, "Upgrading database from version "
+                + oldVersion + " to " + newVersion
+                + ", which will destroy all old data");
+        database.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
+        onCreate(database);
+    }
+
+}
